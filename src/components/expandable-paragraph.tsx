60217e5ae48ad151785872a5cca30c81a60f8c0d
Matthew Button <hello@mattbutton.dev>
2024-09-01 19:52:11 +0100

    feat: add expandable paragraph component

---
+++ src/components/expandable-paragraph.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "./ui";
+
+interface ExpandableParagraphProps {
+  className?: string;
+  text: string;
+  limit?: number;
+}
+
+const ExpandableParagraph = ({
+  className,
+  text,
+  limit = 255,
+}: ExpandableParagraphProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const content = isExpanded ? text : text.slice(0, limit);
+  const isTruncated = text.length > limit;
+
+  return (
+    <p className={className}>
+      {content}
+      {isTruncated && !isExpanded ? "... " : " "}
+      {isTruncated && !isExpanded && (
+        <Button variant="link" onClick={() => setIsExpanded(true)}>
+          Read more
+        </Button>
+      )}
+      {isExpanded && (
+        <Button variant="link" onClick={() => setIsExpanded(false)}>
+          Show less
+        </Button>
+      )}
+    </p>
+  );
+};
+
+export default ExpandableParagraph;
