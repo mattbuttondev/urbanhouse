b82e226bcb833c9a23dec3e73e176b942580219b
Matthew Button <hello@mattbutton.dev>
2024-09-01 19:51:59 +0100

    feat: add rating component

---
+++ src/components/rating.tsx
@@ -0,0 +1,16 @@
+import { StarFill } from "./icons";
+
+interface RatingProps {
+  rating: number;
+}
+
+const Rating = ({ rating }: RatingProps) => {
+  return (
+    <div className="flex items-center gap-1">
+      <StarFill className="text-yellow-500" />
+      <p className="font-medium">{rating}</p>
+    </div>
+  );
+};
+
+export default Rating;
